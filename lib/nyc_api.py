6896cc729f9ad33a436ca6323831a90d8ee453c4
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-28 16:07:18 -0500

    completed labwork

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,20 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program['agency'])
+
+    return programs_list
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+  print(school)
